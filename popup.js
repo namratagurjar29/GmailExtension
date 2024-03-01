114eb4bec7c2bdf23783b453336eb51a7d70d135
namratagurjar01 <gurjarnamrata0@gmail.com>
2024-03-01 10:11:00 +0530

    Gmailextension

--- popup.js
+++ popup.js
@@ -1,19 +1,46 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const sendButton = document.getElementById('sendButton');
-    const recipientInput = document.getElementById('recipient');
 
-    sendButton.addEventListener('click', function () {
-        const recipient = recipientInput.value;
-        if (recipient) {
-            sendBulkEmail(recipient);
-        } else {
-            alert('Please enter a recipient.');
-        }
-    });
+function signIn(){
+    let oauth2EndPints="https://accounts.google.com/o/oauth2/auth"
 
-    function sendBulkEmail(recipient) {
-        // Implement the logic to send bulk emails using the Gmail API
-        // You would integrate with the Gmail API here
-         console.log(`Sending email to ${recipient}`);
+    let form =document.createElement('form');
+    form.setAttribute('method','GET');
+    form.setAttribute('action',oauth2EndPints);
+
+    let params={
+        "client_id":"269520431952-4j62sqmrhgg56dv0mf8nia7b2htjj48a.apps.googleusercontent.com",
+        "redirect_uri":"http://localhost:5500/GmailExtension/",
+        "response_type":"token",
+        "scope":"https://www.googleapis.com/oauth2/v1/certs",
     }
-});
+}
+    // Function to send emails using Gmail API
+    function sendEmailUsingGmailAPI(message) {
+        // Replace this with your actual code to send emails using Gmail API
+        // Example:
+        return fetch('https://accounts.google.com/o/oauth2/auth', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ${accessToken}',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                raw: base64EncodedEmail(message)
+            })
+        })
+        .then(response => response.json());
+    }
+
+    // Function to encode email message to base64 (required by Gmail API)
+    function base64EncodedEmail(message) {
+        // Convert the message to RFC 2822 formatted string
+        const rfc2822Message = To: ${message.to}\r\n +
+                               Subject: ${message.subject}\r\n\r\n +
+                               ${message.body};
+        
+        // Convert the RFC 2822 message to base64
+        const encodedMessage = btoa(rfc2822Message);
+
+        return encodedMessage;
+    };
+
+
